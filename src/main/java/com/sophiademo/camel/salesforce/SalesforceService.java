16f0943c1dfa86119b924745f0035c3109bb696c
Jorge Cano <jorge.canoc@gmail.com>
2016-03-04 12:19:48 -0600

    JACC

--- src/main/java/com/sophiademo/camel/salesforce/SalesforceService.java
+++ src/main/java/com/sophiademo/camel/salesforce/SalesforceService.java
@@ -2,18 +2,27 @@ package com.sophiademo.camel.salesforce;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.apache.camel.salesforce.dto.QueryRecordsAccount;
+import org.apache.camel.salesforce.dto.QueryRecordsOpportunity;
 
 
 @Path("/sf")
 public class SalesforceService {
 
 	@GET
-	@Path("/{test}")
+	@Path("/opportunities")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getOpportunities(@PathParam("test") Long test){
+	public QueryRecordsOpportunity getOpportunities(){
 		return null;
 	}
+
+	@GET
+	@Path("/accounts")
+	@Produces(MediaType.APPLICATION_JSON)
+	public QueryRecordsAccount getAccounts(){
+		return null;
+	}	
 }
