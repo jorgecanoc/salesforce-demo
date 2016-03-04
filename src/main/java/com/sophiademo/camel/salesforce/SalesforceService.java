960d0735648caa0d1d3bf9f52c9fdb637462f686
Jorge Cano <jorge.canoc@gmail.com>
2016-03-04 13:20:58 -0600

    JACC

--- src/main/java/com/sophiademo/camel/salesforce/SalesforceService.java
+++ src/main/java/com/sophiademo/camel/salesforce/SalesforceService.java
@@ -2,27 +2,41 @@ package com.sophiademo.camel.salesforce;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.camel.salesforce.dto.QueryRecordsAccount;
 import org.apache.camel.salesforce.dto.QueryRecordsOpportunity;
 
-
 @Path("/sf")
 public class SalesforceService {
 
 	@GET
-	@Path("/opportunities")
+	@Path("/opportunity")
 	@Produces(MediaType.APPLICATION_JSON)
-	public QueryRecordsOpportunity getOpportunities(){
+	public QueryRecordsOpportunity getOpportunities() {
 		return null;
 	}
 
 	@GET
-	@Path("/accounts")
+	@Path("/opportunity/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public QueryRecordsAccount getAccounts(){
+	public QueryRecordsOpportunity getOpportunity(@PathParam("id") String id) {
 		return null;
-	}	
+	}
+
+	@GET
+	@Path("/account")
+	@Produces(MediaType.APPLICATION_JSON)
+	public QueryRecordsAccount getAccounts() {
+		return null;
+	}
+
+	@GET
+	@Path("/account/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public QueryRecordsAccount getAccount(@PathParam("id") String id) {
+		return null;
+	}
 }
